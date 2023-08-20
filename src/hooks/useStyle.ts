59b6a382f442e8801b150c29a7b9e44b7aaf8bd2
chsdwn <chasedown@hotmail.com>
2023-08-20 18:59:59 +0300

    fix(useStyle): fix callback return type

--- src/hooks/useStyle.ts
+++ src/hooks/useStyle.ts
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 
 export const useStyle = <TStyle extends ViewStyle | ImageStyle | TextStyle>(
-  styleCb: () => StyleProp<TStyle>,
+  styleCb: () => StyleProp<TStyle extends (infer U)[] ? U : TStyle>,
   deps?: DependencyList,
 ) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
