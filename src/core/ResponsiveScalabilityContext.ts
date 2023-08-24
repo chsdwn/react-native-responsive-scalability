92761e2080a9c5bdb4b480e9eb511698bf01ef3a
chsdwn <chasedown@hotmail.com>
2023-08-24 17:41:17 +0300

    fix(ResponsiveScalabilityContext): add default sm and md breakpoint values

--- src/core/ResponsiveScalabilityContext.ts
+++ src/core/ResponsiveScalabilityContext.ts
@@ -1,11 +1,14 @@
 import { createContext } from 'react';
 
-import { BASE_HEIGHT, BASE_WIDTH } from '../config';
+import { BASE_HEIGHT, BASE_WIDTH, BREAKPOINTS } from '../config';
 import { IResponsiveScalabilityContext } from '../types';
 
 export const ResponsiveScalabilityContext =
   createContext<IResponsiveScalabilityContext>({
     baseWidth: BASE_WIDTH,
     baseHeight: BASE_HEIGHT,
-    breakpoints: {},
+    breakpoints: {
+      sm: BREAKPOINTS.sm,
+      md: BREAKPOINTS.md,
+    },
   });
